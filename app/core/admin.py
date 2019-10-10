dfc758446751defbe17553a4e7dde73773ecffa9
Avi Levi <avilevi@Avis-MacBook-Pro.local>
2019-10-10 12:16:04 +0300

    Added endpoint for creating users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,19 +8,21 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        # how page gonna look
-        # (header , {fields to show}
+        #  The way user page in admin tool will be
+        # (title , {fields to show}
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
         (_('Permissions'),
          {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
+
+    #  The field that need to be provided in add user
     add_fieldsets = (
         (None, {
             'classes': ('wide',),  # wide is the default class
-            'fields': ('email', 'password1', 'password2')}
-         ),
+            'fields': ('email', 'password1', 'password2')
+        }),
     )
 
 

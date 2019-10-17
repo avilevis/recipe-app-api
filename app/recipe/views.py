fe7654c86ef5b1dbed18d1c11aac8a654b73a068
Avi Levi <avilevi@Avis-MacBook-Pro.local>
2019-10-17 10:41:25 +0300

    Added list recipe feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,7 +3,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -45,3 +45,15 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
         else:
             serializer.save(user=self.request.user)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
